d7a0ad93c8642c2a7d29b0c9afe44f98ef57238d
nathanaelambert <163461244+nathanaelambert@users.noreply.github.com>
2024-11-12 20:59:11 +0100

    roberta

--- src/embedding_models/roberta.py
+++ src/embedding_models/roberta.py
@@ -1,8 +1,8 @@
 from sentence_transformers import SentenceTransformer
 
-class All_MiniLM_L6_v2:
+class Roberta:
     def __init__(self):
-        self.model_name = 'stsb-roberta-large'
+        self.model_name = 'roberta'
         self.model_info = "A sentence transformer optimized for generating embeddings of short paragraphs; It will use only the first 256 tokens of the input text."
         self.model =  SentenceTransformer('stsb-roberta-large')
 
